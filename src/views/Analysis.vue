8c33b8d45e06244b2bfd4dd52e1e5ac1c31a6018
Melvin Chua <chua0946@e.ntu.edu.sg>
2020-06-22 14:58:36 +0800

    added olt img from api to analysis

--- src/views/Analysis.vue
+++ src/views/Analysis.vue
@@ -1,22 +1,48 @@
 <template>
-<v-container>
-    <h5>Olt A Power 1 Month</h5>
-    <v-img src="http://dev1.ectivisecloud.com:8081/images/olt-a-pwr-1m.jpg"></v-img>
-    <h5>Olt A Power 1 Week</h5>
-    <v-img src="http://dev1.ectivisecloud.com:8081/images/olt-a-pwr-1w.jpg"></v-img>
-</v-container>
+  <v-container fluid>
+    <v-row>
+      <v-col>
+        <v-card>
+          <v-card-title>
+            <h3>OLT A</h3>
+          </v-card-title>
+          <v-card-text>
+            <h5>Olt A Power 1 Day</h5>
+            <v-img src="http://dev1.ectivisecloud.com:8081/images/olt-a-pwr-24h.jpg"></v-img>
+            <h5>Olt A Power 1 Week</h5>
+            <v-img src="http://dev1.ectivisecloud.com:8081/images/olt-a-pwr-1w.jpg"></v-img>
+            <h5>Olt A Power 1 Month</h5>
+            <v-img src="http://dev1.ectivisecloud.com:8081/images/olt-a-pwr-1m.jpg"></v-img>
+            <h5>Olt A Power 1 Year</h5>
+            <v-img src="http://dev1.ectivisecloud.com:8081/images/olt-a-pwr-1y.jpg"></v-img>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-card>
+          <v-card-title>
+            <h3>OLT B</h3>
+          </v-card-title>
+          <v-card-text>
+            <h5>Olt B Power 1 Month</h5>
+            <v-img src="http://dev1.ectivisecloud.com:8081/images/olt-b-pwr-1m.jpg"></v-img>
+            <h5>Olt B Power 1 Year</h5>
+            <v-img src="http://dev1.ectivisecloud.com:8081/images/olt-b-pwr-1y.jpg"></v-img>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-
 export default {
-    name:"analysis",
-    components:{
-        
-    }
-}
+  name: "analysis",
+  components: {}
+};
 </script>
 
 <style>
-
 </style>
