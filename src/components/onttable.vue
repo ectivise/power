27a3cae43bdccdf7ea69fd29d97e90875c8bae4c
Melvin Chua <chua0946@e.ntu.edu.sg>
2020-06-24 16:23:35 +0800

    align to left

--- src/components/onttable.vue
+++ src/components/onttable.vue
@@ -13,7 +13,7 @@
             class="elevation-1"
           >
             <template v-slot:item="{item}">
-              <tr @click="handlerow(item)">
+              <tr @click="handlerow(item)" class="text-left">
                 <td><v-chip :color="getColor(item.device.redundant)" dark class="caption" small>{{ item.device.redundant }}</v-chip> {{item.device.name}}</td>
                 <td>{{item.OLTport}}</td>
                 <td>{{item.optical.distance}}</td>
